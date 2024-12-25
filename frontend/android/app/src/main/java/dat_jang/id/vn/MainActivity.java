a0597247e3863007bde9973604e8187e844b9533
Đỗ Long Ánh <98139573+Jangkuz@users.noreply.github.com>
2024-12-25 19:35:49 +0700

    add: config for building to android app

---
+++ frontend/android/app/src/main/java/dat_jang/id/vn/MainActivity.java
@@ -0,0 +1,5 @@
+package dat_jang.id.vn;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dat_jang.id.vn',
+  appName: 'DATJ_app_frontend',
+  webDir: 'dist'
+};
+
+export default config;
